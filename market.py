58204d11b4b49bbdfeeccb0614d26fadf7ee0d67
João Guilherme Schmidt <joaogschmidt261103@gmail.com>
2023-11-07 01:35:55 -0300

    commit

--- market.py
+++ market.py
@@ -1,1 +1,12 @@
-from 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
